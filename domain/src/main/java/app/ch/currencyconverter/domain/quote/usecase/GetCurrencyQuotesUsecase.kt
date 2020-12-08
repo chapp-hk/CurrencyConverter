84a590172630b878bc783f53a8605ee95563ca85
alanwong <alan.wongchiho@gmail.com>
2020-12-08 15:22:16 +0800

    fix: wrong repository class injected in quotes usecase

--- domain/src/main/java/app/ch/currencyconverter/domain/quote/usecase/GetCurrencyQuotesUsecase.kt
+++ domain/src/main/java/app/ch/currencyconverter/domain/quote/usecase/GetCurrencyQuotesUsecase.kt
@@ -2,16 +2,15 @@ package app.ch.currencyconverter.domain.quote.usecase
 
 import app.ch.currencyconverter.domain.base.Error
 import app.ch.currencyconverter.domain.base.Result
-import app.ch.currencyconverter.domain.currency.entity.CurrencyCode
-import app.ch.currencyconverter.domain.currency.repository.CurrencyRepository
+import app.ch.currencyconverter.domain.quote.repository.QuoteRepository
 import javax.inject.Inject
 
 class GetCurrencyQuotesUsecase @Inject
 constructor(
-    private val getCurrencyRepository: CurrencyRepository
+    private val quoteRepository: QuoteRepository
 ) {
 
-    suspend fun execute(): Result<List<CurrencyCode>, Error> {
-        return getCurrencyRepository.getCurrencyCodes()
+    suspend fun execute(): Result<Map<String, Double>, Error> {
+        return quoteRepository.getCurrencyQuotes()
     }
 }
